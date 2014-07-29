28ad12d7ec8398516f8eb8d83a4cc1a1970e3e0d
fabio914 <fabio914@gmail.com>
2014-07-29 00:21:27 -0300

    Initial Commit

---
+++ bbCodeString/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bbCodeString
+//
+//  Created by Fabio Dela Antonio on 7/29/14.
+//  Copyright (c) 2014 Bluenose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bbCodeString/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  bbCodeString
+//
+//  Created by Fabio Dela Antonio on 7/29/14.
+//  Copyright (c) 2014 Bluenose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
